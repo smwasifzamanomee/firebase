4ff5be6a18ad80fd57d64a32a41e6806cf9c3109
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-05-24 22:45:01 +0600

    signup form setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import SignUp from "./component/SignUp";
+
 export default function App() {
   return (
     <h1 className="text-3xl font-bold underline">
-      Hello world!
+      <SignUp/>
     </h1>
   )
 }

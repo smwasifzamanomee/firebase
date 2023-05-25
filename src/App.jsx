c850bfe6b8e34c2eaab0a4f41631e0bc2f55b1d7
smwasifzamanomee <wasifbdjsr@gmail.com>
2023-05-26 00:36:29 +0600

    firebase auth google and email half done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { initializeApp } from 'firebase/app';
-// import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 import firebaseConfig from './firebase/firebase.config';
 
-
 const App = () => {
-  const [user, setUser] = useState(null);
-  console.log(user)
+  const [newUser, setNewUser] = useState(false);
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: '',
+  });
 
   useEffect(() => {
-    // Initialize Firebase app
-
     initializeApp(firebaseConfig);
 
-    // Listener for changes in user authentication state
     const auth = getAuth();
     auth.onAuthStateChanged((user) => {
       setUser(user);
@@ -28,7 +30,7 @@ const App = () => {
       .then(() => {
         getRedirectResult(auth)
           .then((result) => {
-            const user = result.user;
+            const { user } = result.user;
             setUser(user);
           })
           .catch((error) => {
@@ -39,35 +41,155 @@ const App = () => {
         console.error(error);
       });
   };
-  
 
   const handleSignOut = () => {
     const auth = getAuth();
     auth.signOut()
       .then(() => {
-        // Sign out successful
-        setUser(null);
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          password: '',
+          photo: '',
+          error: '',
+          success: false
+        };
+        setUser(signedOutUser);
       })
       .catch((error) => {
-        // An error occurred during sign out
         console.error(error);
       });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(user.email, user.password);
+
+    if (newUser && user.email && user.password) {
+      console.log('submitting')
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          // Signed in 
+          const newUserInfo = { ...user };
+          newUserInfo.error = '';
+          newUserInfo.success = true;
+          setUser(newUserInfo);
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const newUserInfo = { ...user };
+          newUserInfo.error = error.message;
+          setUser(newUserInfo);
+          // ..
+        });
+    }
+
+    if (!newUser && user.email && user.password) {
+      const auth = getAuth();
+      signInWithEmailAndPassword(auth, user.email, user.password)
+        .then((userCredential) => {
+          // Signed in 
+          const user = userCredential.user;
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+        });
+    };
+  }
+
+  const handleChange = (e) => {
+    let isFieldValid;
+
+    if (e.target.name === 'email') {
+      const isEmailValid = /\S+@\S+\.\S+/.test(e.target.value);
+      isFieldValid = isEmailValid;
+      if (isFieldValid) {
+        const newUserInfo = { ...user };
+        newUserInfo[e.target.name] = e.target.value;
+        setUser(newUserInfo);
+      }
+    }
+  };
+
+  const handleChange1 = (e) => {
+    let isFieldValid;
+
+    if (e.target.name === 'password') {
+      const isPasswordValid = e.target.value.length > 6;
+      const passwordHasNumber = /\d{1}/.test(e.target.value);
+      isFieldValid = isPasswordValid && passwordHasNumber;
+      if (isFieldValid) {
+        const newUserInfo = { ...user };
+        newUserInfo[e.target.name] = e.target.value;
+        setUser(newUserInfo);
+      }
+    }
+  };
+
+  if (user === null) {
+    // Render a loading state or spinner while Firebase initializes
+    return <p>Loading...</p>;
+  }
+
+
+
   return (
-    <div>
-      {user ? (
+    <div className='flex justify-center items-center h-screen gap-20'>
+      {user.isSignedIn ? (
         <div>
-          <h2>Welcome, {user.displayName}!</h2>
-          <img className='rounded-full' src={user.photoURL} alt={user.displayName} />
+          <h2>Welcome, {user.name}!</h2>
+          <img className='rounded-full' src={user.photo} alt={user.name} />
           <button onClick={handleSignOut}>Sign Out</button>
         </div>
       ) : (
-        <div className='flex justify-center items-center h-screen'>
+        <div>
           <p className='text-blue-500'>Sign In with Google</p>
-          <button className='border border-black rounded' onClick={handleSignIn}>Sign In</button>
+          <button className='' onClick={handleSignIn}>
+            Sign In
+          </button>
         </div>
       )}
+      <br />
+      <div className=''>
+        <h1 className='text-2xl font-bold'>Our own authentication</h1>
+        <input type="checkbox" onChange={() => setNewUser(!newUser)} name="newUser" id="" />
+        <label htmlFor="newUser">New User Sign Up</label>
+        <form onSubmit={handleSubmit}>
+          {newUser && <input
+            name="text"
+            type="name"
+            placeholder="Enter your Name"
+            className='py-4 p-4 mb-4 border rounded-xl border-black'
+          />}
+          <br />
+          <input
+            name="email"
+            onChange={handleChange}
+            type="email"
+            placeholder="Enter your email"
+            className='py-4 p-4 mb-4 border rounded-xl border-black'
+          />
+          <br />
+          <input
+            name="password"
+            onChange={handleChange1}
+            type="password"
+            placeholder="Enter your password"
+            className='py-4 p-4 mb-4 border rounded-xl border-black'
+          />
+          <br />
+          <input type="submit" value="Submit" className='border border-orange-500' />
+        </form>
+        <p className='text-red-500'>{user.error}</p>
+        {
+          user.success && <p className='text-green-500'>User created successfully</p>
+        }
+      </div>
     </div>
   );
 };
